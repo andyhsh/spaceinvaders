8b2959d55a556fa0cf57ab06bdc49af8448e48a5
Andy Ho <89andy@gmail.com>
2017-02-24 14:59:49 +0800

    cleaned up files

--- gameboard.js
+++
@@ -1,9 +0,0 @@
-//get height and width of gameboard
-var screenHeight = document.getElementById('gameboard').clientHeight;
-var screenWidth = document.getElementById('gameboard').clientWidth;
-
-//create gameboard object
-var gameboard = {
-	height: screenHeight,
-	width: screenWidth
-}
